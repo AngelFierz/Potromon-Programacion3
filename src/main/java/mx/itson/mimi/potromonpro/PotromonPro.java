7323cab4cfbc427cc48f270ef53001461ceecb24
Propietario <Propietario@DESKTOP-1G4VLT2>
2024-12-02 16:56:40 -0700

    comienzo

---
+++ src/main/java/mx/itson/mimi/potromonpro/PotromonPro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.itson.mimi.potromonpro;
+
+/**
+ *
+ * @author Propietario
+ */
+public class PotromonPro {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
