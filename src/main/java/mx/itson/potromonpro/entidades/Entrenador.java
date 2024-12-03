d4f58e5928a30d6ecf0b863b296f3f88e155f9d6
Propietario <Propietario@DESKTOP-1G4VLT2>
2024-12-02 19:11:50 -0700

    agregado entrenador.java test

--- src/main/java/mx/itson/potromonpro/entidades/Entrenador.java
+++ src/main/java/mx/itson/potromonpro/entidades/Entrenador.java
@@ -9,5 +9,69 @@ package mx.itson.potromonpro.entidades;
  * @author dzlan
  */
 public class Entrenador {
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apodo
+     */
+    public String getApodo() {
+        return apodo;
+    }
+
+    /**
+     * @param apodo the apodo to set
+     */
+    public void setApodo(String apodo) {
+        this.apodo = apodo;
+    }
+
+    /**
+     * @return the imagen
+     */
+    public String getImagen() {
+        return imagen;
+    }
+
+    /**
+     * @param imagen the imagen to set
+     */
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
     
-}
+    private int id;
+    private String nombre;
+    private String apodo;
+    private String imagen;  
+
+    
+    }
+
+
