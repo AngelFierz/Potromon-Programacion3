6f2801a1ecd567efcea0beea76ff5c07325e53df
dzlan <dzlan@LAPTOP-J5UU8NIQ>
2024-12-02 23:37:04 -0600

    Se agregaron los métodos en la clase Entrenador

--- src/main/java/mx/itson/potromonpro/entidades/Entrenador.java
+++ src/main/java/mx/itson/potromonpro/entidades/Entrenador.java
@@ -7,6 +7,9 @@ package mx.itson.potromonpro.entidades;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import mx.itson.potromonpro.persistencia.Conexion;
 
 /**
@@ -77,6 +80,28 @@ public class Entrenador {
     private String imagen;  
 
     
+    public static List<Entrenador> getAll(){
+        List<Entrenador> entrenadores = new ArrayList<>();
+        try {
+            Connection conexion = Conexion.obtener();
+            Statement statement = conexion.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT id_responsable, nombre, apodo, imagen FROM entrenador");
+            while(rs.next()){
+                Entrenador r = new Entrenador();
+                r.setId(rs.getInt(1));
+                r.setNombre(rs.getString(2));
+                r.setApodo(rs.getString(3));
+                r.setImagen(rs.getString(4)); 
+                entrenadores.add(r);
+            }
+        } catch(Exception ex){
+            System.err.println("Ocurrió un error: " + ex.getMessage());
+        }
+        return entrenadores;
+    }   
+    
+    
+    
         public static Entrenador getById(int id){
         Entrenador r = new Entrenador();
         
@@ -101,9 +126,66 @@ public class Entrenador {
     }
     
     
+
+    public static boolean save(String nombre, String apodo, String imagen){
+        boolean resultado = false;
+        try{
+            Connection conexion = Conexion.obtener();
+            String consulta = "INSERT INTO entrenador (nombre, apodo,imagen) VALUES (?, ?, ?)";
+            PreparedStatement statement = conexion.prepareStatement(consulta);
+            statement.setString(1, nombre);
+            statement.setString(2, apodo);
+            statement.setString(3, imagen);
+            
+            statement.execute();
+            resultado = statement.getUpdateCount() == 1;
+            conexion.close();
+        }catch(Exception ex){
+            System.err.println("Ocurrió un error: " + ex.getMessage());
+        }
+        return resultado;
+    }
     
     
+        public static boolean edit(int id, String nombre, String apodo, String imagen){
+        boolean resultado = false;
+        try{
+            Connection conexion = Conexion.obtener();
+            String consulta = "UPDATE entrenador SET nombre = ?, apodo = ?, imagen = ? WHERE id = ?";
+            PreparedStatement statement = conexion.prepareStatement(consulta);
+            statement.setString(1, nombre);
+            statement.setString(2, apodo);
+            statement.setString(3, imagen);
+            statement.setInt(4, id);
+            
+            statement.execute();
+            resultado = statement.getUpdateCount() == 1;
+            conexion.close();
+        }catch(Exception ex){
+            System.err.println("Ocurrió un error: " + ex.getMessage());
+        }
+        return resultado;
+    }  
     
+    
+        public static boolean delete(int id){
+        boolean resultado = false;
+        try{
+            Connection conexion = Conexion.obtener();
+            String consulta = "DELETE FROM entrenador WHERE entrenador_id=?";
+            PreparedStatement statement = conexion.prepareStatement(consulta);
+            statement.setInt(1, id);
+            
+            statement.execute();
+            resultado = statement.getUpdateCount() == 1;
+            conexion.close();
+        }catch(Exception ex){
+            System.err.println("Ocurrió un error: " + ex.getMessage());
+        }
+        return resultado;
+    }         
+         
+         
     
     }
 
